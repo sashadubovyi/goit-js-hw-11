37c7536912f0ba8bb7e5c9edf1c11b925e125aab
Oleksandr Dubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-05-10 19:47:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,3 @@ import { loadMorePictures } from './js/searchPic';
 refs.btnLoadMore.style.visibility = 'hidden';
 refs.form.addEventListener('submit', searchInputPictures);
 refs.btnLoadMore.addEventListener('click', loadMorePictures);
-
-export const state = {
-  currentPage: 1,
-  perPage: 40,
-  totalHits: null,
-  symbol: '',
-};
